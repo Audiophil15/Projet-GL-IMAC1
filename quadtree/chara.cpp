a6657222185515113b647c49663f3382da0ab7c0
Laurence <wanglaurence13@gmail.com>
2022-05-12 20:52:09 +0200

    delete

--- quadtree/chara.cpp
+++
@@ -1,7 +0,0 @@
-#include "chara.h"
-
-Chara::Chara():numberChara(0){}
-
-Chara::Chara(int nbChara):numberChara(nbChara){}
-
-Chara::Chara(int nbChara, int slChara):numberChara(nbChara), selectedChara(slChara){}
